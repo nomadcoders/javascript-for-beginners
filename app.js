7e814cd66126d99361c495511f149edc5b998f32
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-05-15 18:09:01 +0900

    3.1 Form Submission

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log("hello", loginInput.value);
+  const username = loginInput.value;
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
