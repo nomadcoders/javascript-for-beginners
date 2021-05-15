01eccd98e4b987b11b0e2556bc70677e84680444
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-05-15 19:47:47 +0900

    3.5 Saving Username

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem("username", username);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
