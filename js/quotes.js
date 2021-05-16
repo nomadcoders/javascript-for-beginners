0c6d246bd1fc5e3a4a55ef00aa08fd1b9420ce94
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-05-16 20:11:21 +0900

    5.0 Quotes

---
+++ js/quotes.js
@@ -0,0 +1,50 @@
+const quotes = [
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote:
+      "The world is a book and those who do not travel read only one page.",
+    author: "Saint Augustine",
+  },
+  {
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller",
+  },
+  {
+    quote: "To Travel is to Live",
+    author: "Hans Christian Andersen",
+  },
+  {
+    quote: "Only a life lived for others is a life worthwhile.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "You only live once, but if you do it right, once is enough.",
+    author: "Mae West",
+  },
+  {
+    quote: "Never go on trips with anyone you do ntot love.",
+    author: "Hemmingway",
+  },
+  {
+    quote: "We wander for distraction, but we travel for fulfilment.",
+    author: "Hilaire Belloc",
+  },
+  {
+    quote: "Travel expands the mind and fills the gap.",
+    author: "Sheda Savage",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
