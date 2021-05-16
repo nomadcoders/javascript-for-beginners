5874df713ba5476174c5df78dc886198c38ef8dc
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-05-16 20:39:19 +0900

    5.1 Background

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
