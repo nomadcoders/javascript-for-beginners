f9a9c5a80623a06787a39bf14d562ef894d4771e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-05-16 19:22:30 +0900

    4.1 Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000);
