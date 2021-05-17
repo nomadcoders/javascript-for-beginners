9b7d3768f0beea0be43b2ee2743e3258d9d95428
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-05-17 13:16:17 +0900

    6.1 Adding ToDos

--- js/quotes.js
+++ js/quotes.js
@@ -29,7 +29,7 @@ const quotes = [
     author: "Mae West",
   },
   {
-    quote: "Never go on trips with anyone you do ntot love.",
+    quote: "Never go on trips with anyone you do not love.",
     author: "Hemmingway",
   },
   {
