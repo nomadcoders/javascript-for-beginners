30d846b82434be567d6d01aa9c779b046e1f948b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-05-17 15:09:03 +0900

    7.0 Geolocation

---
+++ js/weather.js
@@ -0,0 +1,10 @@
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log("You live in", lat, lng);
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
