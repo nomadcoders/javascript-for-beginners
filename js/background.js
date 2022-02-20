ffe3679a52deb600cdd9a1223af6f1b194ad5678
tenderdaz <99939615+tenderdaz@users.noreply.github.com>
2022-02-20 22:03:11 +0900

    Delete js directory

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
